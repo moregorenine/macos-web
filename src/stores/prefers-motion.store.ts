7f33637d0220c4fac2743adeed01c145898dfbdf
moregorenine <moregorenine@gmail.com>
2024-11-22 00:18:53 +0900

    Updates

--- src/stores/prefers-motion.store.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte-local-storage-store';
-
-export const prefersReducedMotion = writable(
-  'macos:is-reduced-motion',
-  matchMedia('(prefers-reduced-motion)').matches,
-);
--- svelte.config.js
+++
@@ -1,7 +0,0 @@
-import sveltePreprocess from 'svelte-preprocess';
-
-export default {
-  preprocess: sveltePreprocess({
-    replace: [['__DATE__', new Date().toISOString()]],
-  }),
-};
